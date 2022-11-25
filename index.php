b6220d069681867e8cf65b80e714da5475ae16c9
weartstudio <balazs@weart.hu>
2022-11-25 13:20:08 +0100

    vctc

--- index.php
+++ index.php
@@ -3,5 +3,26 @@
 $context          = Timber::context();
 $context['posts'] = new Timber\PostQuery();
 $context['post'] = new Timber\Post();
+//
+$context['cats'] = Timber::get_terms('category');
+
+$context['title'] = "";
+if ( is_day() ) {
+	$context['title'] = 'Archívum: '.get_the_date( 'D M Y' );
+} else if ( is_month() ) {
+	$context['title'] = 'Archívum: '.get_the_date( 'M Y' );
+} else if ( is_year() ) {
+	$context['title'] = 'Archívum: '.get_the_date( 'Y' );
+} else if ( is_tag() ) {
+	$context['title'] = single_tag_title( '', false );
+} else if ( is_category() ) {
+	$context['title'] = single_cat_title( '', false );
+} else if ( is_tax() ){
+	$term = get_queried_object(); // Is this the appropriate way to do it?
+	$context['title'] = $term->name;
+} else if ( is_post_type_archive() ) {
+	$context['title'] = post_type_archive_title( '', false );
+}
+
 
 Timber::render( 'index.twig', $context );
